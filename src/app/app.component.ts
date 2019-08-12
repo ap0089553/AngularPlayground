b0730a1f59c078d8c8909220c3da2bb17d7716eb
ani <ap0089553@gmail.com>
2019-08-12 17:18:55 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
-  feature = 'receipe';
+  featureSelected = 'receipe';
   onSelect(feature: string) {
-    this.feature = feature;
+    this.featureSelected = feature;
   }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +6,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Dal Khichadi', 'Great Indian Mixture', 'https://asmallbite.com/wp-content/uploads/2018/05/Khichdi-Recipe-1.jpg'),
     new Recipe('Dosa', 'Great Indian Snack', 'https://recipes.timesofindia.com/thumb/54289752.cms?imgsize=495844&width=800&height=800')
@@ -13,6 +15,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
